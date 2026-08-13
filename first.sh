37c89294f4f64ac5afa8d35aa2927b3224d3fc61
Adhrit <adhritsigdel@gmail.com>
2026-08-13 23:10:58 +0545

    Bash practice

--- first.sh
+++ first.sh
@@ -1,1 +1,19 @@
-v
+y=-1
+x="Adhrit"
+echo "Hello $x"
+printf "Your age is %d\n" "$y"
+if [ $y -ge 100 ]; then
+    echo "You are adult"
+elif [ "$y" -ge 18 ]; then
+    echo "You are dead"
+elif (("$y" <= 0)); then
+    echo "You are not born"
+else
+    echo "You are young"
+fi
+
+greet(){
+    echo hello $1 and $2
+}
+
+greet Adhrit Luffy
